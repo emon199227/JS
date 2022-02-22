d2e8d6665fcb2735542a7a06311b38e828fb5e5f
emon199227 <emon199227@gmail.com>
2022-02-22 23:52:29 +0600

    JS data type function

---
+++ number method --to fixed -- to prcision/num_method_to_fixed_to prcision.js
@@ -0,0 +1,35 @@
+
+    //number convert to string 
+    var num = 20;
+    num = toString(num);
+    console.log(typeof(num));
+
+   //String comvert to number or integer 
+    var another_String="20";
+    another_String=parseInt(another_String);
+    console.log(typeof(another_String));
+
+    //using fixed function
+
+    var num_one = 2.5678;
+
+    console.log(num_one.toFixed(2));
+
+        //using fixed Precision
+
+        var num_Precesion = 2.5678;
+
+        console.log(num_Precesion.toPrecision(1));
+
+        //direct convert data type
+
+        console.log(Number(" 10 "));
+        console.log(Number("10.25"));
+
+   
+
+        //boolen daya type 
+        console.log(Number(true));
+        console.log(Number(false));
+
+    
