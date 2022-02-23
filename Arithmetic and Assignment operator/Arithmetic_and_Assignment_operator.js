71eb27365683c8cb1755886add9abd939bfbb03d
emon199227 <emon199227@gmail.com>
2022-02-23 23:33:43 +0600

    arithmetic and assisment operator

---
+++ Arithmetic and Assignment operator/Arithmetic_and_Assignment_operator.js
@@ -0,0 +1,20 @@
+//arithmetic operator 
+var x=5,y=10;
+result1=x-y;
+result2=x*y;
+result3=x/y;
+result4=x%y;
+result5=x**3;
+result6=x+y;
+document.write("arithmetic :::<br/>sub : "+result1+"<br/>add : "+result6+"<br/>sub : "+result2+"<br/>Division : "+result3+"<br/>Modulas : "+result4+"<br/>Power : "+result5);
+//assignment operator
+var a=5,b=10;
+a+=b;
+b-=a;
+var m=5,n=10;
+m*=n;
+n/=m;
+var l=5,k=10;
+l%=k;
+k**=l;
+document.write("assisment  :::<br/>print a+=b : "+a+"<br/> print b-=a : "+b+"<br/>print m*=n :"+m+"<br/>n/-=m :"+n+"l%=k :"+l+"<br/>k**=l :"+k);
