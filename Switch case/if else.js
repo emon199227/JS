95490bc314f4641a79a95544e0534c820896fc16
emon199227 <emon199227@gmail.com>
2022-03-04 01:24:17 +0600

    Switch Case

---
+++ Switch case/if else.js
@@ -0,0 +1,47 @@
+//digit spelling 
+//0-zero,1-one,9-nine , 10-- not a digit
+var digit = prompt("Enter a Digit : ");
+if (digit==0)
+ {
+   document.write("Press Zero"); 
+}
+else if (digit==1)
+{
+    document.write("Press One ");  
+}
+else if (digit==2)
+{
+    document.write("Press Two ");  
+}
+else if (digit==3)
+{
+    document.write("Press Three ");  
+}
+else if (digit==4)
+{
+    document.write("Press Four ");  
+}
+else if (digit==5)
+{
+    document.write("Press Five ");  
+}
+else if (digit==6)
+{
+    document.write("Press Six ");  
+}
+else if (digit==7)
+{
+    document.write("Press Seven ");  
+}
+else if (digit==8)
+{
+    document.write("Press Eight ");  
+}
+else if (digit==9)
+{
+    document.write("Press Nine ");  
+}
+else
+{
+    document.write("Not a Digit");
+}
---
+++ Switch case/switch_case.js
@@ -0,0 +1,63 @@
+//switch case use 
+var digit = prompt("Enter a digit : ");
+switch (digit) {
+    case "0":
+        {
+            document.write("Your Digit Zero");
+            break;
+        }
+        case "1":
+        {
+            document.write("Your Digit One");
+            break;
+        }
+        case "2":
+        {
+            document.write("Your Digit Two");
+            break;
+        }
+        case "3":
+        {
+            document.write("Your Digit Three");
+            break;
+        }
+        case "4":
+        {
+            document.write("Your Digit Four");
+            break;
+        }
+        case "5":
+        {
+            document.write("Your Digit Five");
+            break;
+        }
+        case "6":
+        {
+            document.write("Your Digit Six");
+            break;
+        }
+        case "7":
+        {
+            document.write("Your Digit Seven");
+            break;
+        }
+        case "8":
+        {
+            document.write("Your Digit Eight");
+            break;
+        }
+        case "9":
+        {
+            document.write("Your Digit Nine");
+            break;
+        }
+
+
+    default:
+        {
+            document.write("Not a Digit ");
+            break;
+
+        }
+        
+}
