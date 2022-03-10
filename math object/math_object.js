38e375d69fcbad3803bb438f2c1d1d79e004dd63
emon199227 <emon199227@gmail.com>
2022-03-10 23:16:25 +0600

    Math object

---
+++ math object/math_object.js
@@ -0,0 +1,10 @@
+//math object 
+var a = Math.sqrt(25);
+var b = Math.abs(-5);
+var sin=Math.sin(2);
+var power =Math.pow(2,4);
+var floor =Math.floor(2.4);
+var ceil = Math.ceil(2.4);
+var max = Math.max(2,3,-6);
+var rand = Math.random();
+document.write("Square : "+a+"</br>Abs : "+b+"</br>sin : "+sin+"</br>Power : "+power+"</br>Floor : "+floor+"</br>Celling  : "+ceil+"</br>Maximum : "+max+"</br>Random : "+rand);
