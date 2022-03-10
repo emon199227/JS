eb5a06f19649b58ad5a2ab076eea2442d9f944da
emon199227 <emon199227@gmail.com>
2022-03-11 01:22:00 +0600

    Date Object and method

---
+++ Date object & method/date_object_method.js
@@ -0,0 +1,12 @@
+//Date Object and date Method 
+var date = new Date();
+document.write("Today Date : "+date+"</br>");
+
+var year = date.getFullYear();
+document.write("</br></br>Today Full year  : "+year+"</br>");
+
+var month = date.getMonth();
+document.write("</br></br>Today Full Month  : "+month+"</br>");
+
+var currentDate=date.getDate();
+document.write("</br></br>Date : "+currentDate+"</br>");
