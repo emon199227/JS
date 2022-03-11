f53e7f875f8955eac45a8235180e383eb6cd15e0
emon199227 <emon199227@gmail.com>
2022-03-12 02:11:23 +0600

    query selector for java script

---
+++ query selector/query_selector.js
@@ -0,0 +1,10 @@
+//query selector
+//normal hyper text change without list 
+document.querySelector("a").innerHTML="hyper  text";
+//list hyper text change 
+document.querySelector("li a").innerHTML="List a text";
+//class access
+document.querySelector(".my-div a").innerHTML="Div a text";
+//query selector all
+document.querySelectorAll("p")[0].innerHTML="THIS PARAGAPH TEXT 1";
+document.querySelectorAll("p")[1].innerHTML="THIS PARAGAPH TEXT 2";
