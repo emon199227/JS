098cb6450dd4f629ad5549584dbfb1cf2c2dc694
emon199227 <emon199227@gmail.com>
2022-03-12 00:56:46 +0600

    Dom Selected HTML

---
+++ dom select html/dom_select_html.js
@@ -0,0 +1,11 @@
+//get element by ID
+var myheading=document.getElementById("heading1");
+myheading.innerHTML="Hello Bangladesh";
+
+//get element by Tag name 
+var myheading2=document.getElementsByTagName("h2")[0].innerHTML="My Bangladesh";
+var myheading3=document.getElementsByTagName("h2")[1].innerHTML="My Bangladesh 2";
+
+//get element by Class name
+var myheading4=document.getElementsByClassName("para1")[0].innerHTML="Mathbaria 1";
+var myheading5=document.getElementsByClassName("para2")[0].innerHTML="Mathbaria 2";
