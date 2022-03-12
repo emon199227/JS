c5190ad5decad77b6ad3d5a92b3bfd9a5f853506
emon199227 <emon199227@gmail.com>
2022-03-13 03:21:15 +0600

    onclick event

---
+++ OnClic Event/onclick_event.js
@@ -0,0 +1,15 @@
+function mymessage()
+{
+    alert("Hello ! EveryOne ");
+}
+
+function mymessage2()
+{
+    var myvar=document.querySelector("#smsid");
+    myvar.innerHTML="You Have Click Button 2 ";
+}
+var mypic=document.querySelector("#myimg");
+function mypicture()
+{
+    mypic.src="images/emon.jpg";
+}
