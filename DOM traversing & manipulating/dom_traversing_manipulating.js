97c38d5e56d63e520b23060998a8713c3c6c7f67
emon199227 <emon199227@gmail.com>
2022-03-13 23:12:17 +0600

    dom travasing & manipulating :

---
+++ DOM traversing & manipulating/dom_traversing_manipulating.js
@@ -0,0 +1,5 @@
+//parent list access then chaild select 
+var studentList=document.querySelector(".student-list");
+var studentA=studentList.querySelector(".student-a");
+//downword
+var studentAA=studentList.children[0];
