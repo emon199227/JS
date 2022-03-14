fd2110b97018d710078cf96bab25e55d690b6ae0
emon199227 <emon199227@gmail.com>
2022-03-15 02:44:47 +0600

    changing css style

---
+++ change css style/change_css_style.js
@@ -0,0 +1,14 @@
+var myvar = document.querySelector("#paraid");
+//style add 
+function addstyle()
+{
+   
+    myvar.classList.add("para-style");
+}
+
+//remove style 
+function removestyle()
+{
+    
+    myvar.classList.remove("para-style");
+}
