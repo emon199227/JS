0cd764f47d907fee1ce9f50c4fac93b59001719e
emon199227 <emon199227@gmail.com>
2022-03-23 00:11:32 +0600

    key press event lister

---
+++ keypress Listener/keypress_listener.js
@@ -0,0 +1,69 @@
+var h1 = document.querySelector("h1");
+var buttonsLength = document.querySelectorAll(".my-button").length;
+//button add lister 
+for(let x=0; x<buttonsLength; x++){
+    document.querySelectorAll(".my-button")[x].addEventListener("click", function(){
+        //h1.innerHTML = this.innerHTML + " is clicked";
+        var text = this.innerHTML;
+		audioplay(text);
+        playAnimation(text);
+        
+
+      
+    });
+    
+}
+document.addEventListener("keypress",function(event){
+    var text=event.key;
+    audioplay(text);
+    playAnimation(text);
+
+
+})
+function audioplay(text)
+{
+	  switch(text)
+        {
+            case "a":
+            {
+                var audio = new Audio('sound/clip_1.mp3');
+                 audio.play();
+                 break;
+
+            }
+                
+            case "b":
+            {
+                var audio = new Audio('sound/clip_2.mp3');
+                audio.play();
+                break;
+
+            }
+                
+            case "c":
+            {
+                var audio = new Audio('sound/clip_3.mp3');
+                audio.play();
+                break;
+    
+            }
+                
+        }
+	
+}
+
+function playAnimation(text)
+{
+   var selectedButton= document.querySelector("."+text);
+   //animation add 
+   selectedButton.classList.add('anim');
+   //remove animation
+   setTimeout(function()
+   {
+    selectedButton.classList.remove("anim"); 
+
+   },1000);
+   
+
+}
+
