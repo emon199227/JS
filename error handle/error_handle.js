0800b39bd8c4a4a7d0ceb8d40d583f6cb0892f3e
emon199227 <emon199227@gmail.com>
2022-04-10 01:27:46 +0600

    error handle

---
+++ error handle/error_handle.js
@@ -0,0 +1,15 @@
+try{
+    alert("This java script page ");
+    alert(x);
+    alert("This the error");
+
+}catch(err){
+    console.log("Error page ");
+    console.log(err);
+    // console.log("error name: "+err.name);
+    // console.log(err.message);
+
+}
+finally{
+    alert("Bye everyone");
+}
