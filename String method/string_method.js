86bf2105f15ab8540686d1c75aedfc37f4d1bee4
emon199227 <emon199227@gmail.com>
2022-04-18 02:15:28 +0600

    String method start with end with include position

---
+++ String method/string_method.js
@@ -0,0 +1,5 @@
+//start with and position
+const message = "Today is friday";
+document.write("Start with :"+message.startsWith('Today',0)+"<br>");
+document.write("End with :"+message.endsWith('friday')+"<br>");
+document.write("Include  :"+message.includes('friday'));
