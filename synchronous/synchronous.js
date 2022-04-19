74f8a29084cb73e999575c2109728cc9268be262
emon199227 <emon199227@gmail.com>
2022-04-20 02:10:30 +0600

    synchronous ES6

---
+++ synchronous/synchronous.js
@@ -0,0 +1,22 @@
+//synchronous programming 
+const taskOne = () => {
+    console.log("Task 1");
+};
+const taskTwo = () => {
+    console.log("Task 2 : Data Loading ");
+};
+const taskThree = () => {
+    console.log("Task 3");
+};
+const taskFour = () => {
+    console.log("Task 4");
+};
+const taskFive = () => {
+    console.log("Task 5");
+};
+//calling function
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
